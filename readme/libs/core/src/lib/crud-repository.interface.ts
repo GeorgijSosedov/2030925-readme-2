640be2555e10c21db09d03f5e5ffcfdee33689f9
GeorgijSosedov <MarmeladOFForiginal@mail.ru>
2022-11-29 00:36:26 +0500

    homework

--- readme/libs/core/src/lib/crud-repository.interface.ts
+++ readme/libs/core/src/lib/crud-repository.interface.ts
@@ -1,5 +1,5 @@
 export interface CRUDRepository<E,I,R> {
-    findById(id: E): Promise<R | null>;
+    findById(id: string): Promise<R | null>;
     create(item: E): Promise<R>;
     update(id: I, item: E): Promise<R>;
     destroy(id:I): Promise<void>;
